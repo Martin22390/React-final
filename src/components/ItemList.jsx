7b3a77dbd66cc92a18bfe4537a41b1c7092dde2b
Martin Romero Arias <martin.romero@bue.edu.ar>
2023-09-21 17:57:46 -0300

    Subiendo modificacion fotos itemlist y css

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -9,27 +9,27 @@ const ItemList = ({ items, isLoading }) => {
 
   return (
     <div className="item-list-container">
-    <h1 className="item-list-title">Productos:</h1>
+      <h1 className="item-list-title">Productos:</h1>
 
-    <ul className="item-list">
-      {items.map((item) => (
-        <li key={item.id} className="item-list-item">
-          <Link to={`/item/${item.id}`} className="item-link">
-            <div className="item-image">
-              <img
-                src={item.image || "/placeholder.png"}
-                alt={item.name}
-              />
-            </div>
-            <h3 className="item-name">{item.title}</h3>
-            <p className="item-price">${item.price}</p>
-            <p className="item-category">{item.categoryId}</p>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+      <ul className="item-list">
+        {items.map((item) => (
+          <li key={item.id} className="item-list-item">
+            <Link to={`/item/${item.id}`} className="item-link">
+              <div className="item-image">
+                <img
+                  src={item.imageId}
+                  alt={item.title}
+                />
+              </div>
+              <h3 className="item-name">{item.title}</h3>
+              <p className="item-price">${item.price}</p>
+              <p className="item-category">{item.categoryId}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 ItemList.propTypes = {
